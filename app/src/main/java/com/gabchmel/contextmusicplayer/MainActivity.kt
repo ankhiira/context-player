8e125f499469d51047ae2faf881745f1465b2e18
Gabriela <xchmel27@stud.fit.vutbr.cz>
2021-02-22 11:30:01 +0200

    Created bottom navigation

--- app/src/main/java/com/gabchmel/contextmusicplayer/MainActivity.kt
+++ app/src/main/java/com/gabchmel/contextmusicplayer/MainActivity.kt
@@ -2,10 +2,22 @@ package com.gabchmel.contextmusicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Bottom navigation
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottom_nav_view)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }

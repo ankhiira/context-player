7b9e02999186040046c3f1a32d70d52b75bba165
Gabriela <xchmel27@stud.fit.vutbr.cz>
2021-06-07 13:17:04 +0300

    Comment model

--- predicitonmodule/src/main/java/com/gabchmel/predicitonmodule/PredictionModel.kt
+++ predicitonmodule/src/main/java/com/gabchmel/predicitonmodule/PredictionModel.kt
@@ -6,17 +6,21 @@ import org.pmml4s.model.Model
 
 class PredictionModel(val context: Context) {
 
+    // Function to predict song on the given input data
     fun predict() {
-        var data = floatArrayOf(0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f)
+        // Input data for the model
+        val input = floatArrayOf(0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f)
 
+        // Load the model from asset folder
         val assetManager = context.assets
         val inStream = assetManager.open("decision_tree.pmml")
 
-//        var model = Model.fromFile("c:\\src\\ContextPlayerPython\\decision_tree.pmml")
-        var model = Model.fromInputStream(inStream)
-        var result: Any = model.predict<Any>(data)
-        println("Result:$result")
-        val result2 =  Gson().toJson(result)
-        println("Result2:$result2")
+        // Create a model from input pmml file
+        val model = Model.fromInputStream(inStream)
+        // Make prediction
+        val result: Any = model.predict<Any>(input)
+        // Convert to JSON format to print out
+        val resultJSON =  Gson().toJson(result)
+        println("Result2:$resultJSON")
     }
 }

4f1d2ad280d4b5cf08d69926409bc78b7c0dcd40
Gabriela Chmelařová <chmelarova.gabik@gmail.com>
2024-05-09 20:50:08 +0200

    Removed unused variable

--- app/src/main/java/com/gabchmel/contextmusicplayer/ui/theme/Color.kt
+++ app/src/main/java/com/gabchmel/contextmusicplayer/ui/theme/Color.kt
@@ -9,6 +9,5 @@ val LightGray = Color(0xFF808080)
 val DarkGray = Color(0xFF1F1F1F)
 val DarkestGray = Color(0xFF0F0F0F)
 
-val Red800 = Color(0xFFFF5581)
 val Pink500 = Color(0xffcb1f64)
 val Pink600 = Color(0xFF2B2B2B)
